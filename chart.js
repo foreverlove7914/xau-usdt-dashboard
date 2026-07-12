130b7232c9a1777237552d2389f7092fb392901e
foreverlove7914 <foreverlove7914@gmail.com>
2026-07-13 00:35:19 +0800

    Update chart.js

--- chart.js
+++ chart.js
@@ -1,54 +1,99 @@
 const chart = LightweightCharts.createChart(
 document.getElementById("chart"),
 {
-    width: window.innerWidth,
-    height: 500,
+width: window.innerWidth,
+height:500,
 
-    layout:{
-        background:{
-            color:"#101827"
-        },
-        textColor:"#ffffff"
-    },
+layout:{
+background:{
+color:"#101827"
+},
+textColor:"#ffffff"
+},
 
-    grid:{
-        vertLines:{
-            color:"#202c3d"
-        },
-        horzLines:{
-            color:"#202c3d"
-        }
-    }
+grid:{
+vertLines:{
+color:"#202c3d"
+},
+horzLines:{
+color:"#202c3d"
+}
+},
+
+timeScale:{
+timeVisible:true
+}
+
 });
 
 
-const candleSeries = chart.addCandlestickSeries();
+const candleSeries = chart.addCandlestickSeries({
 
+upColor:"#00ff88",
+downColor:"#ff4444",
+borderVisible:false,
+wickUpColor:"#00ff88",
+wickDownColor:"#ff4444"
+
+});
+
+
+
+let candles = {};
 
 const ws = new WebSocket(
-"wss://fstream.binance.com/ws/xauusdt@kline_1m"
+"wss://stream.bybit.com/v5/public/linear"
 );
 
 
-ws.onmessage = (event)=>{
 
-const data = JSON.parse(event.data);
+ws.onopen = ()=>{
 
-const candle = data.k;
+document.getElementById("status").innerHTML="Connected";
+
+ws.send(JSON.stringify({
+
+op:"subscribe",
+
+args:[
+"kline.1.XAUTUSDT"
+]
+
+}));
+
+};
 
 
-candleSeries.update({
 
-time: candle.t / 1000,
+ws.onmessage=(event)=>{
 
-open: parseFloat(candle.o),
 
-high: parseFloat(candle.h),
+const data=JSON.parse(event.data);
 
-low: parseFloat(candle.l),
 
-close: parseFloat(candle.c)
+if(!data.data) return;
 
-});
+
+let k=data.data[0];
+
+
+let candle={
+
+time:k.start/1000,
+
+open:parseFloat(k.open),
+
+high:parseFloat(k.high),
+
+low:parseFloat(k.low),
+
+close:parseFloat(k.close)
+
+};
+
+
+
+candleSeries.update(candle);
+
 
 };
