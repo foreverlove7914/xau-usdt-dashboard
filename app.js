a753dab4252129e021e73898d100408c998e5b0c
foreverlove7914 <foreverlove7914@gmail.com>
2026-07-12 23:31:00 +0800

    Refactor price handling in WebSocket connection

--- app.js
+++ app.js
@@ -1,27 +1,45 @@
-const price = document.getElementById("livePrice");
+const livePrice = document.getElementById("livePrice");
 const status = document.getElementById("status");
+
+let oldPrice = 0;
 
 const ws = new WebSocket(
 "wss://fstream.binance.com/ws/xauusdt@trade"
 );
 
 ws.onopen = () => {
-status.innerHTML="Connected";
+ status.innerHTML = "Connected";
 };
 
-ws.onmessage = (event)=>{
 
-let data = JSON.parse(event.data);
+ws.onmessage = (event) => {
 
-price.innerHTML =
-"$ " + Number(data.p).toFixed(2);
+ const data = JSON.parse(event.data);
+
+ const price = parseFloat(data.p);
+
+ livePrice.innerHTML = "$" + price.toFixed(2);
+
+
+ if(oldPrice > 0){
+
+   if(price > oldPrice){
+      livePrice.style.color = "#00ff88";
+      status.innerHTML = "📈 PRICE NAIK";
+   }
+
+   else if(price < oldPrice){
+      livePrice.style.color = "#ff4444";
+      status.innerHTML = "📉 PRICE TURUN";
+   }
+
+ }
+
+ oldPrice = price;
 
 };
 
-ws.onerror = ()=>{
-status.innerHTML="Error";
-};
 
-ws.onclose = ()=>{
-status.innerHTML="Disconnected";
+ws.onclose = () => {
+ status.innerHTML="Disconnected";
 };
